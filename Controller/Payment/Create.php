d7169f9588e146e0e043637e7b9f13911a85ed47
Daan Rijpkema <info@daanrijpkema.com>
2021-03-10 17:34:30 +0100

    Many small changes and refactoring

--- Controller/Payment/Create.php
+++ Controller/Payment/Create.php
@@ -10,44 +10,78 @@ use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Sales\Model\Order;
+use stdClass;
 
 class Create extends Action
 {
-    /** @var Session */
-    private $checkoutSession;
+    /**
+     * @var \Magento\Sales\Model\OrderFactory
+     */
+    protected $orderFactory;
 
     /**
-     * @param Context $context
-     * @param CreateHostedCheckout $hostedCheckout
-     * @param Session $checkoutSession
+     * @var \PHPCuong\OnePay\Helper\Data
+     */
+    protected $onePayHelperData;
+
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
+    protected $resultJsonFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Sales\Model\OrderFactory $orderFactory
+     * @param \PHPCuong\OnePay\Helper\Data $onePayHelperData
+     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
      */
     public function __construct(
-        Context $context,
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Sales\Model\OrderFactory $orderFactory,
+        \PHPCuong\OnePay\Helper\Data $onePayHelperData,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
         Session $checkoutSession
     ) {
+        $this->orderFactory = $orderFactory;
+        $this->onePayHelperData = $onePayHelperData;
+        $this->resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
-        $this->checkoutSession = $checkoutSession;
+        
+
+        
+        // $this->checkoutSession = $checkoutSession;
+        // $order = $this->checkoutSession->getLastRealOrder();
     }
 
     /**
-     * Initialize redirect to bank
+     * Place Order action
      *
-     * @return \Magento\Framework\Controller\ResultInterface
+     * @return \Magento\Framework\Controller\Result\JsonFactory
      */
     public function execute()
     {
+        $data = [
+            'error' => true,
+            'message' => __('Order ID no longer exist.')
+        ];
+        $result = $this->resultJsonFactory->create();
+        if ($this->getRequest()->isAjax()
+            && $this->getRequest()->getMethod() == 'POST'
+        ) {
 
-        // echo " HI" ; die();
-        /** @var Order $order */
-        $order = $this->checkoutSession->getLastRealOrder();
+            // $orderId = (int)$this->getRequest()->getParam('order_id');
+            // $orderObject = $this->orderFactory->create()->load($orderId);
 
-        /** do stuff here */
-var_dump($order);
-// die();
-        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setUrl('https://google.com');
 
-        return $resultRedirect;
+            $paymentUrl = "https://google.com";
+            if (true) {
+                $data['error'] = false;
+                $data['message'] = __('Retrieve the payment URL successfully.');
+                $data['payment_url'] = $paymentUrl;
+            }
+        }
+
+        return $result->setData($data);
     }
+
 }
--- view/frontend/web/js/view/payment/method-renderer/epayment-method.js
+++ view/frontend/web/js/view/payment/method-renderer/epayment-method.js
@@ -1,14 +1,17 @@
 /* Based on  php-cuong/magento-offline-payments  */
 define([
     'Magento_Checkout/js/view/payment/default',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    'Magento_Checkout/js/action/redirect-on-success',
+    'mage/url',
     'jquery',
     'mage/validation'
-], function (Component, $) {
+], function (Component, additionalValidators, redirectOnSuccessAction, urlBuilder, $) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'Bluem_Integration/payment/pdqpayment-form',
+            template: 'Bluem_Integration/payment/epayment-form',
             assistantId: ''
         },
 
@@ -27,7 +30,7 @@ define([
             return {
                 method: this.item.method,
                 'additional_data': {
-                    'assistant_id': this.assistantId()
+                    'assistant_id': typeof this.assistantId() !== "undefined" ? this.assistantId() : ""
                 }
             };
         },
@@ -36,9 +39,58 @@ define([
          * @return {jQuery}
          */
         validate: function () {
-            var form = 'form[data-role=pdqpayment-form]';
+            var form = 'form[data-role=epayment-form]';
 
             return $(form).validation() && $(form).validation('isValid');
+        },
+
+        
+        /**
+         * Place order.
+         */
+        placeOrder: function (data, event) {
+            var self = this;
+
+            if (event) {
+                event.preventDefault();
+            }
+            console.log("PLACING ORDER VIA BLUEM");
+
+            if (this.validate() && additionalValidators.validate()) {
+                this.isPlaceOrderActionAllowed(false);
+
+                this.getPlaceOrderDeferredObject()
+                    .fail(function () {
+                            self.isPlaceOrderActionAllowed(true);
+                    })
+                    .done(function(orderID) {
+                        console.log('Order placed, with ID ',orderID)
+                        console.log("Lets go pay!")
+                        $.ajax({
+                            url: urlBuilder.build('bluem/payment/create'),
+                            data: {'order_id': orderID},
+                            dataType: 'json',
+                            type: 'POST'
+                        }).done(function (response) {
+                            console.log("Response from Payment controller")
+                            console.log(response)'
+                            '
+                            if (!response.error) {
+                                window.location.replace(response.payment_url);
+                                self.redirectAfterPlaceOrder = false;
+                            } else {
+                                redirectOnSuccessAction.execute();
+                            }
+                        }).fail(function (response) {
+                            console.log(response);
+                            redirectOnSuccessAction.execute();
+                        });
+
+                        self.afterPlaceOrder();
+                    });
+                return true;
+            }
+
         }
     });
 });
