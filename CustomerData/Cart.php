a335cb846e2721279cbd26f73ade7d5a1a1c114c
Peter Meester <p.meester@meesterdevelopment.nl>
2022-04-04 01:57:16 +0200

    Check if identity check is enabled and scenario

--- CustomerData/Cart.php
+++ CustomerData/Cart.php
@@ -12,7 +12,7 @@ namespace Bluem\Integration\CustomerData;
 use Magento\Customer\CustomerData\SectionSourceInterface;
 
 /**
- * Example data source
+ * Cart data source
  */
 class Cart extends \Magento\Checkout\CustomerData\Cart implements SectionSourceInterface
 {
@@ -21,13 +21,24 @@ class Cart extends \Magento\Checkout\CustomerData\Cart implements SectionSourceI
      */
     public function getSectionData()
     {
-        /**
-         * TODO; Check if age verification is turned on.
-         */
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        
+        $bluem_module_enabled = $objectManager->get('Magento\Framework\App\Config\ScopeConfigInterface')->getValue('integration/general/enable');
+        
+        $bluem_identity_scenario = $objectManager->get('Magento\Framework\App\Config\ScopeConfigInterface')->getValue('integration/identity/identity_scenario');
+        
+        $age_verification_enabled = false;
+        
+        if ($bluem_module_enabled == '1') {
+            if ($bluem_identity_scenario != '0') {
+                $age_verification_enabled = true;
+            }
+        }
+        
         $totals = $this->getQuote()->getTotals();
         $subtotalAmount = $totals['subtotal']->getValue();
         return [
-            'age_verification_enabled' => 'yes',
+            'age_verification_enabled' => $age_verification_enabled === true ? 'yes' : 'no',
             'summary_count' => $this->getSummaryCount(),
             'subtotalAmount' => $subtotalAmount,
             'subtotal' => isset($totals['subtotal'])
