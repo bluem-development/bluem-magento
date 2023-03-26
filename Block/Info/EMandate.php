116c32e11beef615088a9641e69ec9c336f2851c
Peter Meester <p.meester@meesterdevelopment.nl>
2023-03-26 14:14:00 +0200

    Added emandates

---
+++ Block/Info/EMandate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Bluem Integration - Magento2 Module
+ * (C) Bluem 2021
+ *
+ * @category Module
+ * @author   Daan Rijpkema <d.rijpkema@bluem.nl>
+ */
+/* Based on  php-cuong/magento-offline-payments  */
+namespace Bluem\Integration\Block\Info;
+
+use Magento\Payment\Block\Info;
+
+class EMandate extends Info
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Bluem_Integration::info/emandate.phtml';
+
+    /**
+     * @return string
+     */
+    public function toPdf()
+    {
+        $this->setTemplate('Bluem_Integration::info/pdf/emandate.phtml');
+        return $this->toHtml();
+    }
+
+    public function getPaymentRequestInfo()
+    {
+        return $this->getInfoData('order_id');
+        // $orderId = $this->getRequest()->getParam('order_id');
+        // $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    }
+}
