8e40c8b87b85ca40a0d17c6abfaef3fded7d1522
Peter Meester <p.meester@meesterdevelopment.nl>
2023-03-26 14:56:24 +0200

    Update eMandates

---
+++ Controller/Adminhtml/Mandates/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bluem\Integration\Controller\Adminhtml\Mandates;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory = false;
+    
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Bluem_Integration::menu');
+        $resultPage->getConfig()->getTitle()->prepend(
+            __('Bluem &middot; Mandates')
+        );
+        return $resultPage;
+    }
+    
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Bluem_Integration::menu');
+    }
+}
