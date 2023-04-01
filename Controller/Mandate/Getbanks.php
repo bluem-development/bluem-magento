24444970e4559201f918839b811cf016a188cd76
Peter Meester <p.meester@meesterdevelopment.nl>
2023-04-01 03:49:35 +0200

    Update bank selector

--- Controller/Mandate/Getbanks.php
+++ Controller/Mandate/Getbanks.php
@@ -8,30 +8,16 @@
  */
 namespace Bluem\Integration\Controller\Mandate;
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
+use Bluem\Integration\Controller\BluemAction;
 
-use Bluem\BluemPHP\Bluem as Bluem;
-
-use stdClass;
-
-class Getbanks extends Action
+class Getbanks extends BluemAction
 {
-    /**
-     * @var Bluem
-     */
-    protected $_bluem;
-
     /**
      * @inheritdoc
      */
     public function execute()
     {
         $banks = [];
-
-        $bluem_config = new stdClass;
-
-        $this->_bluem = new Bluem($bluem_config);
 
         $bics = $this->_bluem->retrieveBICCodesForContext('Mandates');
         var_dump($bics);
--- Controller/Payment/Getbanks.php
+++ Controller/Payment/Getbanks.php
@@ -6,32 +6,20 @@
  * @category Module
  * @author   Daan Rijpkema <d.rijpkema@bluem.nl>
  */
-namespace Bluem\Integration\Controller\Mandate;
+namespace Bluem\Integration\Controller\Payment;
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
+use Bluem\Integration\Controller\BluemAction;
 
 use Bluem\BluemPHP\Bluem as Bluem;
 
-use stdClass;
-
-class Getbanks extends Action
+class Getbanks extends BluemAction
 {
-    /**
-     * @var Bluem
-     */
-    protected $_bluem;
-
     /**
      * @inheritdoc
      */
     public function execute()
     {
         $banks = [];
-
-        $bluem_config = new stdClass;
-
-        $this->_bluem = new Bluem($bluem_config);
 
         $bics = $this->_bluem->retrieveBICCodesForContext('Mandates');
         var_dump($bics);
