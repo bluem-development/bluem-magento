81538bd350b49490c117c27c6107bc69f9af6dfe
Peter Meester <p.meester@meesterdevelopment.nl>
2023-05-07 15:50:18 +0200

    Added some tests

---
+++ Test/Integration/MyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bluem\Integration\Test\Integration;
+
+class MyTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    public function testSomething()
+    {
+        // Test code here
+    }
+}
---
+++ Test/Unit/MyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bluem\Integration\Test\Unit;
+
+class MyTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSomething()
+    {
+        // Test code here
+    }
+}
