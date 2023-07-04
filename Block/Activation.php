942dc02dd98959bf389f17b7373bb83ee60477ad
Peter Meester <p.meester@meesterdevelopment.nl>
2023-07-04 23:41:32 +0200

    Update activation page

--- Block/Activation.php
+++ Block/Activation.php
@@ -4,18 +4,27 @@ namespace Bluem\Integration\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Data\Form\FormKey;
 
 class Activation extends Template
 {
+    /**
+     * @var $formKey
+     */
+    protected $formKey;
+
     /**
      * Constructor
      * 
      * @param Context $context
      */
     public function __construct(
-        Context $context
+        Context $context,
+        FormKey $formKey,
+        array $data = []
     ) {
-        parent::__construct($context);
+        $this->formKey = $formKey;
+        parent::__construct($context, $data);
     }
 
     /**
@@ -24,5 +33,13 @@ class Activation extends Template
     public function getFormAction()
     {
         return $this->getUrl('bluemadmin/activation/savedata', ['_secure' => true]);
+    }
+
+    /**
+     * Get form key
+     */
+    public function getFormKey()
+    {
+        return $this->formKey->getFormKey();
     }
 }
